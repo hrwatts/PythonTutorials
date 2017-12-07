e4586acf365ed9422b263f0e3a296c5ccfbbb87f
civrev <civrev@gmail.com>
2017-12-07 16:45:05 -0500

    sql started

---
+++ Databases/pets.sql
@@ -0,0 +1,42 @@
+# A simple starter database if you've just installed MySQL on your device and have no DBs to use.
+# This is on MySQLs website for tutorials
+# https://dev.mysql.com/doc/mysql-getting-started/en/
+# you can do this through MySQL in terminal by running this script
+# use source pets.sql
+
+DROP DATABASE IF EXISTS pets;
+CREATE DATABASE pets;
+
+USE pets;
+
+CREATE TABLE cats
+(
+  id              INT unsigned NOT NULL AUTO_INCREMENT, # Unique ID for the record
+  name            VARCHAR(150) NOT NULL,                # Name of the cat
+  owner           VARCHAR(150) NOT NULL,                # Owner of the cat
+  birth           DATE NOT NULL,                        # Birthday of the cat
+  PRIMARY KEY     (id)                                  # Make the id the primary key
+);
+
+#add some cats
+INSERT INTO cats ( name, owner, birth) VALUES
+  ( 'Whiskers', 'Brandon', '2015-01-03' ),
+  ( 'Socks', 'Casey', '2013-11-13' ),
+  ( 'Snow Ball', 'Jacob', '2016-05-21' );
+
+CREATE TABLE dogs
+(
+  id              INT unsigned NOT NULL AUTO_INCREMENT, # Unique ID for the record
+  name            VARCHAR(150) NOT NULL,                # Name of the dog
+  owner           VARCHAR(150) NOT NULL,                # Owner of the dog
+  birth           DATE NOT NULL,                        # Birthday of the dog
+  breed		  VARCHAR(50),				# Breed of dog optional
+  PRIMARY KEY     (id)                                  # Make the id the primary key
+);
+
+#add some dogs
+INSERT INTO dogs ( name, owner, birth, breed) VALUES
+  ( 'Spot', 'Brandon', '2005-11-22', 'Poodle' ),
+  ( 'Sir Bark', 'Kyle', '2010-12-03', 'Corgi' ),
+  ( 'Peaches', 'Kim', '2013-06-14', 'Husky' );\h
+
--- Databases/sql_basics.py
+++ Databases/sql_basics.py
@@ -1,13 +1,48 @@
 #sql_basics.py
 #based on a course I got from DataCamp.com "Introduction to Databases in Python"
-#SQLAlchemy basics
+#SQLAlchemy basics, connections, reflection objects
 
 #create_engine is what you use to connect with a database
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData, Table
 
-#supply create_engine() with a string that represents the connection
-engine = create_engine("")
+#supply create_engine() with a string that represents the connection URL
+#this is what the connection URL looks like
+# dialect+driver://username:password@host:port/database
+#what this means is
+#dialect = what kind of SQL DB is it? MS SQL, MySQL, PostGRE, etc.
+#driver = SQLAlchemy runs over other python packages so that the end user (me) can use
+#a common interface between them. The python connector MySQL offers, is what I'll use
+#also, don't put your passwords online
+
+#to be able to run this, use the pets.sql file to make a simple test DB on your local host
+
+engine = create_engine("mysql+mysqlconnector://USERNAME:PASSWORD@localhost/pets")
 
 #and then to make connections to the DB you need to do this
 #it is worth noting that it won't actually make a connection until it is being used
-connection = engine.connect()
+connection = engine.connect() #connection made like this, for testing
+
+#you'll use the engine conceptually like your database
+#let's see what tables are in our DB
+print(engine.table_names())
+
+#you want to work without the server as much as you can, considering the speed of calculations
+#would be awful. So what we do is build a python object on our end that mirrors the DB
+#this is called reflection, and it will allow us to stop making connections to use data
+
+#initialize, just to make it easier to read the code
+#MetaData is all the info about what kind of data is stored in a table
+metadata = MetaData()
+
+#this is our reflected object, essentially is is the cats table in the DB, except in our local memory
+#autoload allows us to go ahead and make the connection now to load this reflected table
+cats = Table('cats', metadata, autoload=True, autoload_with=engine)
+
+#note: the defualt print out of a Table is terrible
+print(cats)
+print(type(cats))
+
+#use repr() function to get an informative print out
+#repr() just is an advanced object printing function in python 
+#that returns printable representation of an object
+print(repr(cats))
