4bdc1f28d7139aeddb45d85a1981203e344bd741
civrev <civrev@gmail.com>
2017-12-18 14:56:23 -0500

    SQL and Pandas

--- Databases/census3.py
+++ Databases/census3.py
@@ -1,10 +1,10 @@
 #census3.py
 #based on a course I got from DataCamp.com "Introduction to Databases in Python"
-#SQLite, Aggregation
+#SQLite, Aggregation, Distinct, Group By
 
 from sqlalchemy import create_engine, Table, MetaData
 from sqlalchemy import select, or_, and_, not_, desc
-from sqlalchemy import func
+from sqlalchemy import func #to use .sum() and .count() don't import those directly, python conflicts
 
 #connect to DB
 engine = create_engine("sqlite:///census.sqlite")
@@ -16,6 +16,12 @@ metadata = MetaData()
 #get the table (there is only one in census.sqlite)
 census = Table('census', metadata, autoload=True, autoload_with=engine)
 
+#you can use .distinct() which is an SQL command, to only grab distinct values
+stmt = select([census.columns.state.distinct()])
+stmt = stmt.order_by(census.columns.state)
+results = connection.execute(stmt).fetchall()
+print("All States:"+'*'*77+"\n",results)
+
 #there are some operations you can do in SQL that are more efficient than doing them in python
 #examples: Count, Sum
 #these two are called aggregation functions, cause they collapse multiple records into one
@@ -26,3 +32,48 @@ stmt = select([func.sum(census.columns.pop2008)])
 results = connection.execute(stmt).fetchall()
 
 print("Sum of US population from the census 2008:",results, 30*'*')
+
+#alternatively, since we know only one value is coming back from this query
+#we can use .scalar() instead of .fetch() methods, it will return just the value, not RowProxy
+results = connection.execute(stmt).scalar()
+print("Scalar Sum of US population from the census 2008:",results, 28*'*')
+
+#you can also use SQL "Group By" statements with aggregate functions
+#Group By is done almost exaclt the same as Order By
+#if using multiple columns to group by, it will do them left to right like order by
+#To use .group_by() the columns must be selected, and must have been grouped or aggregated
+stmt = select([census.columns.sex, func.sum(census.columns.pop2008)])
+stmt = stmt.group_by(census.columns.sex)
+results = connection.execute(stmt).fetchall()
+print("2008 Population Grouped By Sex:",results, "*"*19)
+
+#huh, slightly more females than males for the US in 2008
+
+#let's demo the multiple group by by grouping population by sex and age
+#columns must be selected
+stmt = select([census.columns.sex, census.columns.age, census.columns.pop2008])
+stmt = stmt.group_by(census.columns.sex, census.columns.age)
+switch=True
+print("2008 US Population grouped by Sex and Age:" + "*"*46)
+for result in connection.execute(stmt):
+	if result[0]!='F' and switch:
+		print("Male Population by Age " + "*"*20)
+		switch = False
+	print(result)
+
+#SQLAlchemy uses place holders for newly generated columns (such as an aggregate of a column)
+#this makes it somewhat unintuitive to use at first
+stmt = select([census.columns.sex, func.sum(census.columns.pop2008)])
+stmt = stmt.group_by(census.columns.sex)
+results = connection.execute(stmt).fetchall()
+print("Result Keys with Place Holder",results[0].keys(), '*'*30) #remember RowProxys have .keys() not ResultSets
+
+#we see that sum_1 is given for the sum of pop2008
+#so if we want to give it a meaningful name, use .label() 
+#to let it know what to change place holder to during the aggregate function
+stmt = select([census.columns.sex, func.sum(census.columns.pop2008).label('pop2008_sum')])
+stmt = stmt.group_by(census.columns.sex)
+results = connection.execute(stmt).fetchall()
+print("Result Keys Labelled",results[0].keys(),"*"*33)
+
+
---
+++ Databases/census4.py
@@ -0,0 +1,39 @@
+#census4.py
+#based on a course I got from DataCamp.com "Introduction to Databases in Python"
+#SQLite, SQLAlchemy+Pandas, plotting results
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from sqlalchemy import create_engine, Table, MetaData
+from sqlalchemy import select, or_, and_, not_, desc
+from sqlalchemy import func
+
+#connect to DB
+engine = create_engine("sqlite:///census.sqlite")
+connection = engine.connect()
+
+#set up metadata
+metadata = MetaData()
+
+#get the table (there is only one in census.sqlite)
+census = Table('census', metadata, autoload=True, autoload_with=engine)
+
+#A pandas DataFrame can take a SQLAlchemy ResultSet as an argument
+#but you will need to supply the column names separately
+#let's take a query we did in census3.py
+stmt = select([census.columns.sex, func.sum(census.columns.pop2008).label('pop2008_sum')])
+stmt = stmt.group_by(census.columns.sex)
+results = connection.execute(stmt).fetchall()
+
+#now we just use the ResultSet as the argument for pandas.DataFrame()
+df = pd.DataFrame(results)
+print(df)
+
+#pandas won't automatically import the column names at this time, so you must do it manually
+#usually with just the keys of the first result
+df.columns = results[0].keys()
+print(df)
+
+df.plot.bar()
+plt.xticks(df.index,df.sex)
+plt.show()
